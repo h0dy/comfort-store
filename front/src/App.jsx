e0f3bc16cfb962840b320a4ad51589d417c7bb15
h0dy <hasolanh@gmail.com>
2025-01-19 18:30:31 +0300

    pages setup

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,69 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Cart,
+  About,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Products,
+  Register,
+  SingleProduct,
+  Orders,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "products/:id",
+        element: <SingleProduct />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+    ],
+  },
+
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
+
 function App() {
-  return <main className=""></main>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- front/tailwind.config.cjs
+++ front/tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [import("@tailwindcss/typography"), import("daisyui")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
